c93ef3c11d9fdaaaa01fdf071b7a5582d34f9c03
Aiman Fatima <aimanfatimadl@gmail.com>
2021-07-06 01:08:26 +0530

    State & useState hook

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,4 @@
-const tasks = [
-    {
-        id:'1',
-        text:'Reminder 1',
-        day:'July 9th at 10 am',
-        reminder:'true'
-    },
-    {
-        id:'2',
-        text:'Reminder 2',
-        day:'July 9th at 11 am',
-        reminder:'true'
-    },
-    {
-        id:'3',
-        text:'Reminder 3',
-        day:'July 9th at 1 pm',
-        reminder:'false'
-    },
-    {
-        id:'4',
-        text:'Reminder 4',
-        day:'July 9th at 2 pm',
-        reminder:'false'
-    }
-]
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
     return (
         <>
             {tasks.map((task) => (
